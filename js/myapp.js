827cb27310e8ed62f607fcfefc573f313cd47a1f
Caitlin <caitlin.colbert@gmail.com>
2015-04-19 13:36:35 -0700

    adding bill total calculations

--- js/myapp.js
+++ js/myapp.js
@@ -5,8 +5,22 @@ $(document).ready(function () {
 
 angular.module('tip-calc', [])
     .controller('myCtrl', function($scope) {
-        $scope.submit = function() {
 
+        $scope.submit = function() {
+            if ($scope.mealForm.$valid) {
+                console.log($scope.meal.meal_price);
+                $scope.meal.tip = $scope.meal.tip_percent / 100 * $scope.meal.meal_price;
+                $scope.meal.tax = $scope.meal.tax_rate / 100 * $scope.meal.meal_price;
+                $scope.meal.meal_total = $scope.meal.meal_price + $scope.meal.tax;
+                $scope.meal.total = $scope.meal.meal_total + $scope.meal.tip;
+                console.log($scope.meal.total);
             }
+        };
+
+        $scope.reset = function() {
+            $scope.meal_price = null;
+            $scope.tax_rate = null;
+            $scope.tip = null;
+           
         };
     });
