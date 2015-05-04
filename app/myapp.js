7a1c821b777c6e049a898595517f45b921592af3
Caitlin <caitlin.colbert@gmail.com>
2015-05-04 14:08:13 -0700

    update

--- app/myapp.js
+++ app/myapp.js
@@ -1,69 +1,92 @@
 angular.module('tip-calc', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider.when('/', {
-            templateUrl : '/index.html',
-        })
-        .when('/meal', {
-            templateUrl : './meal.html',
-            controller : 'mealCtrl',
-        })
-        .when('/earnings', {
-            templateUrl : './earnings.html',
-            controller : 'earningsCtrl',
-        })
-        .otherwise({ redirectTo : '/index.html' });
+
+.value('earnings', [])
+
+.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl : '/index.html',
     })
+    .when('/meal', {
+        templateUrl : './meal.html',
+        controller : 'mealCtrl',
+    })
+    .when('/earnings', {
+        templateUrl : './earnings.html',
+        controller : 'earningsCtrl',
+    })
+    .otherwise({ redirectTo : '/index.html' });
+})
 
-    .controller('mealCtrl', function($scope) {
-        function init() {
-            $scope.meal = {};
-            $scope.earnings = {};
-            $scope.total = {};
-            $scope.meal.tip = 0;
-            $scope.meal.tax = 0;
-            $scope.meal.meal_total = 0;
-            $scope.meal.total = 0;
-            $scope.earnings.tips = 0;
-            $scope.earnings.meal_count = 0;
-            $scope.earnings.average = 0;
+.controller('mealCtrl', function($scope, earnings) {
+    function init() {
+        $scope.meal = {};
+        $scope.earnings = {};
+        $scope.total = {};
+        $scope.meal.tip = 0;
+        $scope.meal.tax = 0;
+        $scope.meal.meal_total = 0;
+        $scope.meal.total = 0;
+    }
+
+    init();
+
+    $scope.submit = function() {
+        if ($scope.mealForm.$valid) {
+            console.log('submitting');
+            compute();
+            addEarnings();
+            $scope.meal.tip_percent = "";
+            $scope.meal.meal_price = "";
+            $scope.meal.tax_rate = "";
         }
+    };
 
+    $scope.reset = function() {
+        $scope.meal = {};
+    };
+
+    $scope.clear = function() {
+        console.log('clear');
         init();
+    };
 
-        $scope.submit = function() {
-            if ($scope.mealForm.$valid) {
-                console.log('submitting');
-                compute();
-                $scope.meal.tip_percent = "";
-                $scope.meal.meal_price = "";
-                $scope.meal.tax_rate = "";
-            }
-        };
+    function compute() {
+        console.log($scope.meal.meal_price);
+        $scope.meal.tip = $scope.meal.tip_percent / 100 * $scope.meal.meal_price;
+        $scope.meal.tax = $scope.meal.tax_rate / 100 * $scope.meal.meal_price;
+        $scope.meal.meal_total = $scope.meal.meal_price + $scope.meal.tax;
+        $scope.meal.total = $scope.meal.meal_total + $scope.meal.tip;
+        console.log($scope.meal.total);
 
-        $scope.reset = function() {
-            $scope.meal = {};
-        };
+        // totals
+        // $scope.earnings.tips += $scope.meal.tip;
+        // $scope.earnings.meal_count++;
+        // $scope.earnings.average = $scope.earnings.tips / $scope.earnings.meal_count;
+    }
 
-        $scope.clear = function() {
-            console.log('clear');
-            init();
-        };
+    function addEarnings() {
+        earnings.push({'tip': $scope.meal.tip, 'meal_total': $scope.meal.meal_total, 'tax': $scope.meal.tax});
+    }
 
-        function compute() {
-            console.log($scope.meal.meal_price);
-            $scope.meal.tip = $scope.meal.tip_percent / 100 * $scope.meal.meal_price;
-            $scope.meal.tax = $scope.meal.tax_rate / 100 * $scope.meal.meal_price;
-            $scope.meal.meal_total = $scope.meal.meal_price + $scope.meal.tax;
-            $scope.meal.total = $scope.meal.meal_total + $scope.meal.tip;
-            console.log($scope.meal.total);
+})
+.controller('earningsCtrl', function($scope, earnings) {
 
-            // totals
-            $scope.earnings.tips += $scope.meal.tip;
-            $scope.earnings.meal_count++;
-            $scope.earnings.average = $scope.earnings.tips / $scope.earnings.meal_count;
-        }
+    function init() {
+        $scope.tip_total = 0;
+        $scope.meal_count = 0;
+        $scope.average = 0;
+    }
 
-    })
-    .controller('earningsCtrl', function($scope) {
+    init(); 
+    
+    console.log(earnings);
+    for (var meal in earnings) {
+        console.log('meal tip: ' + earnings[meal].tip, 'meal_total: ' + earnings[meal].meal_total);
+        $scope.tip_total += earnings[meal].tip;
+        console.log($scope.tip_total);
+        $scope.meal_count++;
+        console.log($scope.meal_count);
+        $scope.average = $scope.tip_total / $scope.meal_count;
+    }
 
-    });
+});
